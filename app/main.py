8a268e4f9855207a00ceab2c084f25d91c70e792
MichaelOdera <michogelira@gmail.com>
2026-07-31 18:19:12 +0300

    update the guidelines and main.py file for the application

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from rag_chain import create_rag_chain
 
 def main():
     print("📄 Loading documents...")
-    chunks = load_and_split_docs("data/company_policy.txt")
+    chunks = load_and_split_docs("data/workers_guidelines.txt")
 
     print("🧠 Creating vector store...")
     vector_db = create_vector_store(chunks)
